920bbd653824080a5c1a457029ccd14dda0aa60c
Raunak <rritesh@deloitte.com>
2020-02-15 18:37:34 +0530

    Serializers Added for Movie, Rating

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,13 +7,9 @@ from .views import *
 app_name = 'api'
 router = routers.DefaultRouter()
 
+router.register('movie', MovieViewSet)
+router.register('rating', RatingViewSet)
+
 urlpatterns = [
 	path('', include(router.urls)),
-
-    # url(r'^$', HomeView.as_view(), name='HomeView'),
-
-    # url(r'^new-claim/$', NewClaimView.as_view(), name='NewClaimView'),
-    # url(r'^dashboard/$', PolicyDashboardView.as_view(), name='PolicyDashboardView'),
-
-    # url(r'^dashboard/policy/(?P<policy>\d+)$', UserDashboardView.as_view(), name='UserDashboardView'),
 ]
