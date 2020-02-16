6f965509b37d8b14813662d09a082ab7f5021319
Raunak <rritesh@deloitte.com>
2020-02-16 20:11:26 +0530

    Functions in a model as variable inside serializers

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,6 +6,21 @@ class Movie(models.Model):
 	title = models.CharField(max_length=32)
 	description = models.TextField(max_length=360)
 
+	def no_of_ratings(self):
+		ratings = Rating.objects.filter(movie=self)
+		return len(ratings)
+
+	def avg_rating(self):
+		ratings = Rating.objects.filter(movie=self)
+		sum = 0
+		for rating in ratings:
+			sum += rating.stars
+
+		if len(ratings) > 0:
+			return sum/len(ratings)
+		else:
+			return None
+
 class Rating(models.Model):
 	movie = models.ForeignKey(Movie,\
 						on_delete=models.CASCADE)
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
+from django.contrib.auth.models import User
 from .models import Movie, Rating
 from .serializers import *
 
@@ -11,9 +12,29 @@ class MovieViewSet(viewsets.ModelViewSet):
 
 	@action(detail=True, methods=['POST'])
 	def rate_movie(self, request, pk=None):
-		# Insert logic
-		response = {'message' : 'Successful'}
-		return Response(response, status=status.HTTP_200_OK)
+		# import pdb; pdb.set_trace();
+		if 'stars' in request.data:
+			movie = Movie.objects.get(id=pk)
+			# user = request.user
+			user = User.objects.get(id=1)
+			stars = request.data['stars']
+
+			try: # Update existing rating
+				rating = Rating.objects.get(user=user.id, movie=movie.id)
+				rating.stars = stars
+				rating.save()
+			except:
+				Rating.objects.create(user=user, movie=movie, stars=stars)
+
+			serializer = RatingSerializer(rating, many=False)
+			response = {
+				'message' : 'Successful',
+				'result' : serializer.data
+			}
+			return Response(response, status=status.HTTP_200_OK)	
+		else:
+			response = {'message' : "Please provide stars"}
+			return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 class RatingViewSet(viewsets.ModelViewSet):
 	queryset = Rating.objects.all()
