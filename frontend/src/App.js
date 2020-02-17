3b4fb345928db47c63ee93ea58c074fa69b997de
Raunak <rritesh@deloitte.com>
2020-02-18 01:21:03 +0530

    Cleanup for IMDB-frontend project start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,10 @@
 import React from 'react';
 import './App.css';
 
-import Header from './components/header';
-import Footer from './components/footer';
-
-function demoAlert(){
-  alert('Demo Alert! Click Found!');
-}
-
-function OurText(){
-	return <p>Filler content</p>
-}
-
-function ShowMessage(props){
-  if(props.toShow){
-    return <h2>Message</h2>
-  }
-  else{
-    return <h2>Forbidden</h2>
-  }
-  
-}
-
 function App() {
   return (
     <div className="App">
-      <Header
-        title='IMDB Sample Project'
-      />
-      <OurText />
-      <ShowMessage toShow={true} />
-      <Footer 
-        trademark='Learned from Udemy'
-        author='Raunak'
-        myalert={demoAlert}
-      />
+        <h1>Imdb-Sample-Project</h1>
     </div>
   );
 }
